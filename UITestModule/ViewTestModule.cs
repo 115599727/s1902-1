09f798fb08471b34205f92bbdbb3603a90b5b306
Meng <52415667@qq.com>
2019-05-27 15:03:46 +0800

    first commit

---
+++ UITestModule/ViewTestModule.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using UITestModule.Views;
+
+namespace UITestModule
+{
+    /// <summary>
+    /// ViewTest.xaml 的交互逻辑
+    /// </summary>
+    public  class ViewTestModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewT));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1Metro/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Modularity;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace WpfApp1Metro
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           //this.r
+        }
+        protected override Window CreateShell()
+        {
+
+            return Container.Resolve<MainWindow>();
+
+        }
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
