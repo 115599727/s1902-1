297a3afbd8751e95fa2f8349458441eaceaabd09
Meng <52415667@qq.com>
2019-06-05 15:09:50 +0800

    ViewTest Up ViewModel

---
+++ UITestModule/ViewModels/ViewTViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITestModule.ViewModels
+{
+    public class ViewTViewModel : BindableBase
+    {
+
+        private string _demoText;
+        public string DemoText
+        {
+            get { return _demoText; }
+            set
+            {
+                SetProperty(ref _demoText, value);
+                // BtnRunDelegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ViewTViewModel()
+        {
+            this._demoText = "Demo Text String";
+
+        }
+    }
+}
